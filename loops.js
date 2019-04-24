3d57458ae11db0cbcedc9b83221df2cb21a2d2ba
Sean Altemose <sean@seanaltemose.com>
2019-04-24 17:46:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,8 +14,12 @@ function doWhileLoop(array){
 };
 
 function forLoop(array){
-  array.push("I am 1 strange loop.")
-  for (let i = 2; i < 25; i++){
+
+  for (let i = 0; i < 25; i++){
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    }
+    else{
     array.push(`I am ${i} strange loops.`)
   };
   return array;
