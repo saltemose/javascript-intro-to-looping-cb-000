7d3fb6888838bc3456d8b4f96c50ca97700d76ed
Sean Altemose <sean@seanaltemose.com>
2019-04-24 17:46:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,8 +19,9 @@ function forLoop(array){
     if (i === 1) {
       array.push("I am 1 strange loop.");
     }
-    else{
+    else {
     array.push(`I am ${i} strange loops.`)
+    }
   };
   return array;
 };
