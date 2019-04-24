ad4ce8750ab58734c4388fbc6c7d30dc421a03ed
Sean Altemose <sean@seanaltemose.com>
2019-04-24 17:37:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -6,15 +6,8 @@ function whileLoop(number){
 };
 
 function doWhileLoop(array){
-  var i = array.length-1;
-
-  function incrementVariable() {
-    i = i - 1;
-    return i;
-  }
-
   do {
     array.pop();
     console.log("I run once regardless.");
-  } while (incrementVariable() >= 0);
+  } while (array.length > 0);
 };
